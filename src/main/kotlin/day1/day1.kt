4bb74c8d1ef890a63a0fb3ee5f3bf4380298bf3b
Calambor <anton.calambor@gmail.com>
2020-12-01 07:01:14 +0100

    Utility input formatter

--- src/main/kotlin/day1/day1.kt
+++ src/main/kotlin/day1/day1.kt
@@ -1,9 +1,10 @@
 package day1
 
 import support.downloadInput
+import support.toInts1
 
 fun main() {
-    val input = downloadInput(1).map { it.toInt() }
+    val input = downloadInput(1).toInts1()
     input.forEach { a ->
         input.forEach { b ->
             input.forEach { c ->
