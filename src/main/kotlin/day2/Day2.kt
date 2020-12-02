6a39856f0f9d730445567e14d6700f1ddd23ff96
Calambor <anton.calambor@gmail.com>
2020-12-02 06:10:14 +0100

    Solution for Day 2

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -3,9 +3,21 @@ package day2
 import support.downloadInput
 
 fun main() {
-    val input = downloadInput(2).map { it.toInt() }
+    val input = downloadInput(2).map { it.split(' ') }
+    var validNum = 0
     input.forEach { a ->
+        val min = a.first().split('-')[0].toInt()
+        val max = a.first().split('-')[1].toInt()
+        val char = a[1][0]
 
+        val pw = a[2]
+        val count = pw.count { it == char }
+        if (count in min..max) {
+            //validNum++ // Part 1
+        }
+        if ((pw[min - 1] == char).xor(pw[max - 1] == char)) {
+            validNum++
+        }
     }
-
+    println(validNum)
 }
