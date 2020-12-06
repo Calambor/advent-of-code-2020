c6fd66e32902e67939ad29600aac6e00a34d4943
Calambor <anton.calambor@gmail.com>
2020-12-06 06:40:59 +0100

    Solution day 6

---
+++ src/main/kotlin/day6/Day6.kt
@@ -0,0 +1,23 @@
+package day6
+
+import support.downloadInput
+
+fun main() {
+    val input = downloadInput(6)
+    val ans = mutableListOf<List<Char>>()
+    var sum = 0
+    input.forEach {
+        if (it.isEmpty()) {
+            sum += ans.reduce { acc, list ->
+                acc.intersect(list).toList()
+            }.size
+            ans.clear()
+        } else {
+            ans.add(it.toList())
+        }
+    }
+    sum += ans.reduce { acc, list ->
+        acc.intersect(list).toList()
+    }.size
+    println(sum)
+}
