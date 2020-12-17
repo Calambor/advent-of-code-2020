ad86cea4bf6666db73eb313da9a44679e7f0ccd3
Calambor <anton.calambor@gmail.com>
2020-12-17 07:21:38 +0100

    Solution Day17

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -3,57 +3,59 @@ package day17
 import support.downloadInput
 
 fun main() {
-
-    //println(solve(downloadInput(17, 1)))
+    println(solve(downloadInput(17, 1)))
     println("--------------------------")
     println(solve(downloadInput(17)))
-
 }
 
 const val duration = 6
 
+/** Might have allocated some extra world size */
 fun solve(input: List<String>): Any {
 
-    val worldSize = input.first().length + 2 * duration +2
+    val worldSize = input.first().length + 2 * duration + 2
 
-    var grid: List<List<MutableList<Int>>> =
+    var grid: List<List<List<MutableList<Int>>>> =
         List(worldSize) {
             List(worldSize) {
-                List(4 + 2 * duration) { 0 }.toMutableList()
+                List(4 + 2 * duration) {
+                    List(4 + 2 * duration) { 0 }.toMutableList()
+                }
             }
         }
-    var alt: List<List<MutableList<Int>>> =
+    var alt: List<List<List<MutableList<Int>>>> =
         List(worldSize) {
             List(worldSize) {
-                List(4 + 2 * duration) { 0 }.toMutableList()
+                List(4 + 2 * duration) {
+                    List(4 + 2 * duration) { 0 }.toMutableList()
+                }
             }
         }
 
     input.forEachIndexed { x, col ->
         col.forEachIndexed { y, active ->
-            grid[x + duration+2][y + duration+2][duration+1] = if (active == '#') 1 else 0
+            grid[x + duration + 1][y + duration + 1][duration + 1][duration + 1] = if (active == '#') 1 else 0
         }
     }
 
     for (step in 0 until duration) {
-        grid.printSlice(duration+1)
-        grid.printSlice(duration)
-        grid.printSlice(duration-1)
-        println()
+        //grid.printSlice(duration+1, duration+1)
+        //println()
+
         grid.step(alt)
         val tmp = grid
         grid = alt
         alt = tmp
     }
 
-    return grid.flatten().flatten().sum()
+    return grid.flatten().flatten().flatten().sum()
 }
 
-fun List<List<MutableList<Int>>>.printSlice(z: Int) {
+fun List<List<List<MutableList<Int>>>>.printSlice(z: Int, d: Int) {
     this.forEach { col ->
         col.forEach {
             print(
-                if (it[z] == 1) {
+                if (it[z][d] == 1) {
                     "#"
                 } else {
                     "."
@@ -64,27 +66,29 @@ fun List<List<MutableList<Int>>>.printSlice(z: Int) {
     }
 }
 
-fun List<List<MutableList<Int>>>.step(outGrid: List<List<MutableList<Int>>>) {
-
-    for (x in 1 until size-1) {
-        for (y in 1 until size-1) {
-            for (z in 1 until 2 * duration+3) {
-                outGrid[x][y][z] = nextState(x, y, z)
+fun List<List<List<MutableList<Int>>>>.step(outGrid: List<List<List<MutableList<Int>>>>) {
+    for (x in 1 until size - 1) {
+        for (y in 1 until size - 1) {
+            for (z in 1 until 2 * duration + 2) {
+                for (d in 1 until 2 * duration + 2) {
+                    outGrid[x][y][z][d] = nextState(x, y, z, d)
+                }
             }
         }
     }
-
 }
 
-fun List<List<MutableList<Int>>>.nextState(x: Int, y: Int, z: Int): Int {
+fun List<List<List<MutableList<Int>>>>.nextState(x: Int, y: Int, z: Int, d: Int): Int {
     val near = subList(x - 1, x + 2).map {
         it.subList(y - 1, y + 2).map {
-            it.subList(z - 1, z + 2)
+            it.subList(z - 1, z + 2).map {
+                it.subList(d - 1, d + 2)
+            }
         }
     }
-    val nearCount = near.flatten().flatten().sum()
+    val nearCount = near.flatten().flatten().flatten().sum()
 
-    return if (this[x][y][z] == 1) {
+    return if (this[x][y][z][d] == 1) {
         if (nearCount in 3..4) { // includes itself
             1
         } else {
@@ -98,6 +102,3 @@ fun List<List<MutableList<Int>>>.nextState(x: Int, y: Int, z: Int): Int {
         }
     }
 }
-
-// Not 293
-// Not 297
